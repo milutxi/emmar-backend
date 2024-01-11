15a416fd18be1ab51ca980e172c9fa84ced42d67
Sheila Aliaga <aliaga_sheila@hotmail.com>
2024-01-11 13:51:48 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express()
+
+//middleware
+app.use(express.json());
+
+//create handlers
+app.use('/', (req, res) => {
+   console.log('Root route hit');
+   res.send("Hello world!")   
+});
+
+//MongoDB Connection through .env file to hide the URL
+const mongoURL = process.env.DB_URL;
+
+if(!mongoURL) throw Error("Missing db url");
+
+mongoose.connect(mongoURL)
+    .then(() => {
+        const port= parseInt(process.env.PORT || '3000');
+        app.listen(port, () => {
+            console.log('Hola Sheila, Server listening on port ' + port);
+        })
+    })
+    
