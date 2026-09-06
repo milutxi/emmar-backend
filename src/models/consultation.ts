064bf2ebeaf45d301aaeabef7f99e3888fa8d482
milutxi <aliaga_sheila@hotmail.com>
2026-09-06 21:28:53 +0200

    add model for consultation

---
+++ src/models/consultation.ts
@@ -0,0 +1,39 @@
+import { Document, Schema, model, Types } from "mongoose";
+
+interface IConsultation extends Document {
+  clientId: Types.ObjectId;
+  consultationTitle: string;
+  consultationText: string,
+  consultationDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const ConsultationSchema = new Schema<IConsultation>({
+  clientId: {
+    type: Schema.Types.ObjectId,
+    ref: "Client",
+    required: true,
+  },
+  consultationTitle: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  consultationText: {
+    required: true,
+    type: String,
+    trim: true,
+  },
+  consultationDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+},{
+  timestamps: true
+});
+
+const Consulation = model<IConsultation>('Consultation', ConsultationSchema);
+
+export default Consulation;
